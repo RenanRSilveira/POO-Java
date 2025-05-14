6e26e309ffa8eb2bfff5508ea5237d1df96be6c8
RenanRSilveira <silveirarenan4@gmail.com>
2025-05-14 19:35:51 -0300

    Nona aula

---
+++ POO9/App.java
@@ -0,0 +1,14 @@
+package POO9;
+
+import POO9.classes.Circulo;
+import POO9.classes.Quadrado;
+
+public class App {
+    public static void main(String[] args) {
+        Quadrado quadrado = new Quadrado(3);
+        System.out.println(quadrado.calcularArea());
+
+        Circulo circulo = new Circulo(2);
+        System.out.println(circulo.calcularArea());
+    }
+}
