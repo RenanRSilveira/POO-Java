4c40c34553d2d792b1524e021901682f80381e24
RenanRSilveira <silveirarenan4@gmail.com>
2025-05-26 16:06:50 -0300

    Iterator

---
+++ POO22/Iterator/App.java
@@ -0,0 +1,32 @@
+package POO22.Iterator;
+
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        HashSet<String> carros = new LinkedHashSet<>();
+
+        carros.add("HRV");
+        carros.add("Polo");
+        carros.add("Golf");
+        carros.add("City");
+        carros.add("Cruze");
+        carros.add("Argo");
+        carros.add("Ka");
+        carros.add("City");
+
+        System.out.println(carros);
+
+        Iterator<String> it = carros.iterator();
+
+        while (it.hasNext()) {
+            String a = it.next();
+            if (a == "Polo") {
+                it.remove();
+            }
+        }
+
+        System.out.println(carros);
+    }
+}
